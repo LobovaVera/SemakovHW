6eaefba5e3e99a73abace2cf5e548bc6f9fa6cba
Vera Lobova <pchilli@yandex.ru>
2022-09-29 20:16:22 +0300

    first task is done

--- Hw1-3/Program.cs
+++ Hw1-3/Program.cs
@@ -1,2 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// предствьте компьютер может только + и -. вычислить 1. умножение двух целых чисел 2. целочисленно еделение двух целых чисел 3. получение остатка от целочисленного деления целых чисел.
+Console.WriteLine("Enter first digit");
+string a = Console.ReadLine()!;
+
+Console.WriteLine("Enter second digit");
+string b = Console.ReadLine()!;
+
+int firstNumber = Convert.ToInt32(a) ;
+int secondNumber = Convert.ToInt32(b);
+int multipResult = 0;
+
+for( int i = 0; i < secondNumber; i++)
+{
+
+multipResult = multipResult + firstNumber;
+
+}
+
+Console.WriteLine(multipResult);
